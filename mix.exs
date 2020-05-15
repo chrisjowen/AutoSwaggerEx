690db1e0f109deec654602f9313a8478515990a3
Chris Owen <chris.j.owen@hotmail.co.uk>
2020-05-15 03:20:22 -0400

    Changeing version

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule AutoSwaggerEx.MixProject do
   def project do
     [
       app: :auto_swagger_ex,
-      version: "0.1.0",
+      version: "0.0.1",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
